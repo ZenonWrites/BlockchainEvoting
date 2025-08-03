ced4c2774d7bf650b050ac93c267788afee0a41b
ZenonWrites <saifsk3429@gmail.com>
2025-08-03 17:47:09 +0530

    Initial commit

--- backend/build.sh
+++ backend/build.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Navigate to the backend folder
+cd backend
+
+# Create virtual environment with Python 3.10
+python3.10 -m venv env
+source env/bin/activate
+
+# Install requirements
+pip install --upgrade pip
 pip install -r requirements.txt
+
+# Run Django commands
 python manage.py migrate
 python manage.py collectstatic --noinput
