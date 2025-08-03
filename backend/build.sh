e2e70068f617ee36eef34f951abc9cbadf4aceae
ZenonWrites <145893524+ZenonWrites@users.noreply.github.com>
2025-08-03 17:50:09 +0530

    Update build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Navigate to the backend folder
+
 cd backend
 
-# Create virtual environment with Python 3.10
+# Python 3.10 venv
 python3.10 -m venv env
 source env/bin/activate
 
-# Install requirements
+# Install dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
 
-# Run Django commands
+
 python manage.py migrate
 python manage.py collectstatic --noinput
